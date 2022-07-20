27987b70daf92e595974cd5f27f6ae3041b096b0
vahra <vahra@DESKTOP-SO0L1VA>
2022-07-20 18:13:33 +0300

    Add project files.

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Class1
+    {
+        public int Calc(int a, int b,string x)
+        {
+            switch(x)
+            {
+                case "/":
+                return a / b;
+                break;
+                case "+":
+                    return a + b;
+                    break;
+                case "*":
+                    return a * b;
+                    break;
+                case "-":
+                    return a - b;
+                    break;
+            }
+            return 0;
+        }
+        
+
+       public enum Errorenum
+        {
+            Wrong_Symbol,
+            Cannot_Divide_into_0,  
+        }
+            
+
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp5;
+
+Class1 class1= new Class1();  
+int a=Convert.ToInt32(Console.ReadLine());
+string x = Console.ReadLine();
+int b=Convert.ToInt32(Console.ReadLine());
+
+class1.Calc(a, b, x);
+if (x == "/" && b == 0)
+{
+    Console.WriteLine(Class1.Errorenum.Cannot_Divide_into_0);
+}
+else if (x != "/"&&x!="+" && x != "-" && x != "*")
+{
+    Console.WriteLine(Class1.Errorenum.Wrong_Symbol + x);
+}
+Console.WriteLine(class1.Calc(a,b,x));
+
